f17916106a55df4c342814d6763f8f01584f8c8f
viktor.pegij <vpegy.work@gmail.com>
2019-08-26 17:40:23 +0300

    Upd (al.reset): pass argv to git

--- functions/g::rec.fish
+++ functions/g::rec.fish
@@ -1,9 +1,9 @@
 function g::rec -d 'Drop all changes and remove untracked files'
     argparse -s 'h/help' -- $argv ^/dev/null
     if set -q _flag_help
-        _g::help rec ''
+        _g::help rec '[<git reset args>]'
         return
     end
-    g reh
+    git reset --hard $argv
     git clean -fd
 end
--- functions/g::reh.fish
+++ functions/g::reh.fish
@@ -1,8 +1,8 @@
 function g::reh -d 'Drop all changes'
     argparse -s 'h/help' -- $argv ^/dev/null
     if set -q _flag_help
-        _g::help reh ''
+        _g::help reh '[<git reset args>]'
         return
     end
-    git reset --hard
+    git reset --hard $argv
 end
